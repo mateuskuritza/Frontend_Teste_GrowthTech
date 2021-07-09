d950a3e81beac97ff3289364c1b863a7aad3d66a
Mateus_Kuritza <mateuskuritza@gmail.com>
2021-07-08 22:51:05 -0300

    navbar layout ok

--- src/assets/styles/global.js
+++ src/assets/styles/global.js
@@ -5,6 +5,8 @@ export default createGlobalStyle`
 * {
   box-sizing: border-box;
   transition: all ease .2s;
+  margin: 0;
+  padding: 0;
 }
 
 strong {
@@ -18,6 +20,7 @@ a {
 
 body {
   font-family: 'Roboto', sans-serif;
+  background-color: ${props => props.theme.colors.background_secondary};
 }
 
 `;
---
+++ src/assets/styles/themes/gray.js
@@ -0,0 +1,8 @@
+export default {
+    title: "gray",
+    colors: {
+        background_primary: "#C0D6DF",
+        background_secondary: "#DBE9EE",
+        background_tertiary: "#4F6D7A"
+    },
+};
